5a472cbe7d9480e440d0356eecfe86037d086f87
Mattiabe98 <49247389+Mattiabe98@users.noreply.github.com>
2021-05-13 16:35:11 +0000

    FeaturesReply + PacketIn handling

--- loadbalancer.py
+++ loadbalancer.py
@@ -3,18 +3,45 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.topology.api import get_all_host
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet, ether_types
+from ryu.lib.packet import tcp, arp, ipv4
 
 class LoadBalancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
-    # CONFIG_DISPATCHER, regole base
+    # CONFIG_DISPATCHER, gestione Features Reply
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        #codice..
-    # MAIN_DISPATCHER, codice controllore
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Manda al controllore tutti i pacchetti
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=1, match=match, instructions=inst) # priorità inferiore?
+        datapath.send_msg(mod)
+
+    # MAIN_DISPATCHER, gestione dell'evento PacketIn
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        #codice..
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        # Estraiamo gli header del pacchetto
+        pkt = packet.Packet(msg.data)
+        #pkt_ethernet = pkt.get_protocol(ethernet.ethernet)     ci serve, dato che stiamo facendo l'hash solo su IP_src e port_src?
+        pkt_ipv4 = pkt.get_protocol(ipv4.ipv4)
+        pkt_tcp = pkt.get_protocol(tcp.tcp)
+        pkt_arp = pkt.get_protocol(arp.arp)
+
+        # Consideriamo solo i pacchetti IPv4 TCP
+        if (pkt_ipv4 is not None and pkt_tcp is not None):
+            # gestione pacchetto..
+        else:
+            return
