dc8bab137e815faa52a9a90cee249ca09c768d07
Patatone <34103646+Patatone@users.noreply.github.com>
2021-05-17 15:00:37 +0200

    Subnet update

--- topology.py
+++ topology.py
@@ -6,22 +6,22 @@ class Topology(Topo):
     def build(self):
  
         # Hosts and switches
-        host1 = self.addHost('H1', ip='10.0.0.1/24', mac="00:00:00:00:00:01")
-        host2 = self.addHost('H2', ip='10.0.0.2/24', mac="00:00:00:00:00:02")
-        host3 = self.addHost('H3', ip='10.0.0.3/24', mac="00:00:00:00:00:03")
-        host4 = self.addHost('H4', ip='10.0.0.4/24', mac="00:00:00:00:00:04")
-        host5 = self.addHost('H5', ip='10.0.0.5/24', mac="00:00:00:00:00:05")
-        server1 = self.addHost('SRV1', ip='10.0.1.1/24', mac="00:00:00:00:01:01")
-        server2 = self.addHost('SRV2', ip='10.0.1.2/24', mac="00:00:00:00:01:02")
+        host1 = self.addHost('H1', ip='10.0.0.1/8', mac="00:00:00:00:00:01")
+        host2 = self.addHost('H2', ip='10.0.0.2/8', mac="00:00:00:00:00:02")
+        host3 = self.addHost('H3', ip='10.0.0.3/8', mac="00:00:00:00:00:03")
+        host4 = self.addHost('H4', ip='10.0.0.4/8', mac="00:00:00:00:00:04")
+        host5 = self.addHost('H5', ip='10.0.0.5/8', mac="00:00:00:00:00:05")
+        server1 = self.addHost('SRV1', ip='10.0.1.1/8', mac="00:00:00:00:01:01")
+        server2 = self.addHost('SRV2', ip='10.0.1.2/8', mac="00:00:00:00:01:02")
         switch1 = self.addSwitch('SW1')
  
         # Links
+        self.addLink(server1, switch1, port2=1, cls=TCLink, bw=10000, delay='1ms')
+        self.addLink(server2, switch1, port2=2, cls=TCLink, bw=10000, delay='1ms')
         self.addLink(host1, switch1, cls=TCLink, bw=1000, delay='5ms')
         self.addLink(host2, switch1, cls=TCLink, bw=1000, delay='5ms')
         self.addLink(host3, switch1, cls=TCLink, bw=1000, delay='5ms')
         self.addLink(host4, switch1, cls=TCLink, bw=1000, delay='5ms')
         self.addLink(host5, switch1, cls=TCLink, bw=1000, delay='5ms')
-        self.addLink(server1, switch1, cls=TCLink, bw=1000, delay='1ms')
-        self.addLink(server2, switch1, cls=TCLink, bw=1000, delay='1ms')
  
 topos = { 'topology': ( lambda: Topology() ) }
